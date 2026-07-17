6bbefda41b91c9286850ab10e0b024f0936c1900
ASoukhteh <asoukhteh@gmail.com>
2026-07-17 11:16:03 +0330

    update responses + add JSONResponse instead of returning just dictionaries

--- core/main.py
+++ core/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query, status, HTTPException
+from fastapi.responses import JSONResponse
 import random
 app = FastAPI()
 
@@ -12,43 +13,44 @@ names_list = [
 ]
 
 # /names (GET(RETRIEVE), POST(CREATE))
-@app.get("/names", status_code=status.HTTP_200_OK)
+@app.get("/names")
 async def retrieve_names_list(q : str | None = Query(default=None, max_length=50)):
+    result = names_list
     if q:
-        return [item for item in names_list if q in item["name"]]
-    return names_list
+        result =  [item for item in names_list if q in item["name"]]
+    return JSONResponse(content=result, status_code=status.HTTP_200_OK)
 
 @app.post("/names")
-async def create_name(name:str, status_code=status.HTTP_201_CREATED):
+async def create_name(name:str):
     name_obj = {"id": random.randint(6, 100), "name": name}
     names_list.append(name_obj)
-    return name_obj
+    return JSONResponse(content=name_obj, status_code=status.HTTP_201_CREATED)
 
 # /names/:id (GET(RETRIEVE), PUT/PATCH(UPDATE), DELETE)
-@app.get("/names/{name_id}", status_code=status.HTTP_200_OK)
+@app.get("/names/{name_id}")
 async def retrieve_name_detail(name_id:int):
     for name in names_list:
         if name["id"] == name_id:
-            return name
+            return JSONResponse(content=name, status_code=status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object didn't find.") 
 
-@app.put("/names/{name_id}", status_code=status.HTTP_200_OK)
+@app.put("/names/{name_id}")
 async def update_name_detail(name_id:int, name:str):
     for item in names_list:
         if item["id"] == name_id:
             item["name"] = name
-            return item
+            return JSONResponse(content=item, status_code=status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object didn't find.") 
 
-@app.delete("/names/{name_id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/names/{name_id}")
 async def delete_name(name_id:int):
     for item in names_list:
         if item["id"] == name_id:
             names_list.remove(item)
-            return {"detail": "object delete successfully."}
+            return JSONResponse(content={"detail": "object delete successfully."}, status_code=status.HTTP_204_NO_CONTENT)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="object didn't find.") 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!"}
+    return JSONResponse(content={"message": "Hello World!"}, status_code=status.HTTP_200_OK)
 
