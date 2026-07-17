05f588af4600979da427793019874760d30fb637
ASoukhteh <asoukhteh@gmail.com>
2026-07-17 09:54:51 +0330

    add get method to app + /names + /names/{id}

---
+++ core/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+names_list = [
+    {"id":1, "name":"ali"},
+    {"id":2, "name":"amir"},
+    {"id":3, "name":"bahram"},
+    {"id":4, "name":"cohon"},
+    {"id":5, "name":"dani"},
+    {"id":6, "name":"ebrahim"},
+]
+
+# /names (GET(RETRIEVE), POST(CREATE))
+@app.get("/names")
+async def retrieve_names_list():
+    return names_list
+
+# /names/:id (GET(RETRIEVE), PUT/PATCH(UPDATE), DELETE)
+@app.get("/names/{id}")
+async def retrieve_name_detail(name_id:int):
+    for name in names_list:
+        if name["id"] == name_id:
+            return name
+    return {"detail": "name didn't find."}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
