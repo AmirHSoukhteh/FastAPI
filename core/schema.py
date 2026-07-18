18e57924dbf6e7523c3b8225e46399975874636b
ASoukhteh <asoukhteh@gmail.com>
2026-07-18 11:50:14 +0330

    validate name with pydantic

--- core/schema.py
+++ core/schema.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class BasePersonSchema(BaseModel):
     name : str
+
+    @field_validator
+    def validate_name(cls, value):
+        if len(value > 32):
+            raise ValueError("Name must not exceed 32 character")
+        if not value.isalpha():
+            raise ValueError("Name must contain only alphabetic character")        
+        return value
 
 class PersonCreateSchema(BasePersonSchema):
     pass
