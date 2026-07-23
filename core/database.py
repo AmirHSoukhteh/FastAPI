b96ad8afbbb078b06ad13d89ba8bed7fb952f813
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-23 09:22:27 +0330

    manage environment and post some sample data to db

--- core/database.py
+++ core/database.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from config import settings
 # ایجاد مدل پایه برای SQLAlchemy
 Base = declarative_base()
 
 # URL پایگاه داده SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver:5432/test.db"
 # SQLALCHEMY_DATABASE_URL = "mysql://user:password@localhost/test.db"
 
 
 # ایجاد موتور برای اتصال به پایگاه داده
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    settings.SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False})
 
 # ساخت session ساز برای تعامل با پایگاه داده
